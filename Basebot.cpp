6b208adaef43fbff4ffcc66d8e27c9ac232b7f51
Vladimir <ve.hodest@gmail.com>
2017-12-31 00:39:07 +0300

    Added base class for bot

---
+++ Basebot.cpp
@@ -0,0 +1,60 @@
+#include "Trepbot.h"
+
+#include <iostream>
+
+Basebot::Basebot(std::string const& telegramToken,
+                 Httper& http,
+                 Escaper& esc)
+    : offset(0),
+      http(http),
+      esc(esc),
+      tUrl(telegramToken, esc) {}
+
+void Basebot::GetInfo(size_t ownId, std::string& name) {
+  Httper::ContainerType data = http.Get(tUrl.GetMe());
+
+  data.push_back(char(0));
+  nlohmann::json answer = nlohmann::json::parse(data.data());
+
+  ownId = answer["result"]["id"].get<size_t>();
+  name = answer["result"]["username"].get<std::string>();
+}
+
+void Basebot::ProcessUpdates() {
+  // Получаем данные из Телеграма
+  http.SetTimeout(60500);
+  std::cout << tUrl.GetUpdates(60, offset) << std::endl;
+  Httper::ContainerType data = http.Get(tUrl.GetUpdates(60, offset));
+
+  data.push_back(char(0));
+  nlohmann::json answer = nlohmann::json::parse(data.data());
+
+  std::cout << "Message: " << std::endl << data.data() << std::endl;
+
+  // Обрабатывем их
+  if (false == answer["ok"].get<bool>()) {
+    std::cout << "Error" << std::endl;
+    return;
+  }
+
+  nlohmann::json result = answer["result"];
+  size_t max(0);
+  for (auto& update : result) {  // перебор элементов секции Update
+    for (auto obj = update.begin(); obj != update.end(); ++obj) {
+      if (obj.key() == "update_id") {  // определение максимального элемента
+        long int id = obj->get<long int>();
+        if (id > max) {
+          max = id;
+        }
+      } else if (obj->is_object()) {  // получение имени объекта
+        auto it = handlers.find(obj.key());
+
+        if (handlers.end() != it) {
+          (this->*(it->second))(*obj);  // вызов хендлера
+        }
+      }
+    }
+  }
+
+  offset = max + 1;
+}
--- Trepbot.h
+++ Trepbot.h
@@ -1,41 +1,19 @@
 #pragma once
 
-#include "Escaper.h"
-#include "Httper.h"
-#include "TelegramUrlBuilder.h"
+#include "Basebot.h"
 #include "YandexUrlBuilder.h"
-#include "json.hpp"
 
 #include <map>
 
-class Trepbot final {
+class Trepbot final: public Basebot {
  public:
   Trepbot(std::string const& telegramToken,
           std::string const& yandexToken,
           Httper& http,
           Escaper& esc);
 
-  Trepbot(Trepbot const&) = delete;
-  Trepbot& operator=(Trepbot const&) = delete;
-
-  void ProcessUpdates();
-  void GetInfo();
-
  private:
-  using Handler = void (Trepbot::*)(nlohmann::json&);
-  using handlersType = std::map<std::string, Handler>;
-  static handlersType handlers;
-
-  Httper& http;
-  Escaper& esc;
-
-  TelegramUrlBuilder tUrl;
   YandexUrlBuilder yaUrl;
-
-  size_t offset;
-
-  size_t ownId;
-  std::string name;
 
   void MessageHandler(nlohmann::json& msg);
   void QueryHandler(nlohmann::json& query);
